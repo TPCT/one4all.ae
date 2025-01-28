c22e5d8804f88bdaca49dfd089cf55a1febeee94
tpct <eslamjackie@outlook.com>
2025-01-28 18:21:14 +0200

    first commit

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -21,15 +21,18 @@ class SetLocale
             return $next($request);
 
         if (in_array($request->segment(1), array_keys(config('app.locales')))){
+            $locale = $request->segment(1);
             session()->put('locale', $request->segment(1));
         }else{
-            if (!session()->has('locale'))
-                session()->put('locale', config('app.locale'));
-            return redirect('/' . session('locale') . "/" . $request->path());
+            $locale = config('app.locale');
+            if (!session()->has('locale')) {
+                session()->put('locale', $locale);
+            }
+            return redirect('/' . $locale . "/" . $request->path());
         }
 
-        app()->setLocale(session('locale'));
-        \URL::defaults(['locale' => session('locale')]);
+        app()->setLocale($locale);
+        \URL::defaults(['locale' => $locale]);
 
         if ($request->get('fallbackPlaceholder'))
             return redirect('/' . $request->get('fallbackPlaceholder'));
