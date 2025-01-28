1c30ce8854136a4584a00800126823f16e3e56c4
tpct <eslamjackie@outlook.com>
2025-01-29 01:39:59 +0200

    first commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,8 +24,14 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\EncryptCookies::class,
+        \App\Http\Middleware\VerifyCsrfToken::class,
         \Illuminate\Session\Middleware\StartSession::class,
-//        SetLocale::class,
+        SetLocale::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        StatusChecker::class
+
     ];
 
     /**
@@ -35,12 +41,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            StatusChecker::class
         ],
 
         'api' => [
