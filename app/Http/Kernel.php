7de711f203e4628447865259cb4b1ae927221b1f
tpct <eslamjackie@outlook.com>
2025-01-29 01:49:02 +0200

    first commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,9 +27,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        SetLocale::class,
     ];
 
     /**
@@ -39,7 +36,9 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-
+            \Illuminate\Session\Middleware\StartSession::class,
+            SetLocale::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
