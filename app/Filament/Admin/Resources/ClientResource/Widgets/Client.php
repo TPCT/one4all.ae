42ed7afeb3534be2bc9a9cb772329c1052d121d9
tpct <eslamjackie@outlook.com>
2025-01-28 15:53:44 +0200

    first commit

--- app/Filament/Admin/Resources/ClientResource/Widgets/Client.php
+++ app/Filament/Admin/Resources/ClientResource/Widgets/Client.php
@@ -5,6 +5,7 @@ namespace App\Filament\Admin\Resources\ClientResource\Widgets;
 use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Database\Query\Builder;
 
 class Client extends BaseWidget
 {
@@ -14,8 +15,19 @@ class Client extends BaseWidget
             Stat::make(__("Clients"), function (){
                 return \App\Models\Client::count();
             })->icon('bi-person-fill'),
-            Stat::make(__("Active Clients"), function(){
-                return \App\Models\Client::where('active', true)->count();
+            Stat::make(__("Subscribed Clients"), function(){
+                return \App\Models\Client::where('active', true)
+                    ->where(function (\Illuminate\Database\Eloquent\Builder $builder){
+                        $builder->whereHas('services');
+                        $builder->orWhereHas('packages');
+                    })->count();
+            })->icon('bi-person-fill'),
+            Stat::make(__("Un-Subscribed Clients"), function(){
+                return \App\Models\Client::where('active', true)
+                    ->where(function (\Illuminate\Database\Eloquent\Builder $builder){
+                        $builder->whereDoesntHave('packages');
+                        $builder->whereDoesntHave('services');
+                    })->count();
             })->icon('bi-person-fill'),
         ];
     }
