6791cc9a7571b108158970995a084f6e42ab133e
tpct <eslamjackie@outlook.com>
2025-01-28 17:09:12 +0200

    first commit

--- app/Filament/Middlewares/AdminPanelVisitor.php
+++ app/Filament/Middlewares/AdminPanelVisitor.php
@@ -16,6 +16,7 @@ class AdminPanelVisitor
      */
     public function handle(Request $request, Closure $next): Response
     {
+        app()->setLocale('en');
         visitor()->visit();
         return $next($request);
     }
