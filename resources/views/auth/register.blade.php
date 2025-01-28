cca57791c486af451c52f3c16484b6aa53c2e580
tpct <eslamjackie@outlook.com>
2025-01-28 15:05:26 +0200

    first commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,8 +21,8 @@
                 @method('POST')
                 <input type="hidden" name="phone_country" id="phone_country">
                 <div class="head-form mb-5">
-                    <h1>{){$register_page_section->title}}</h1>
-                    {!'! $register_page_section->description !!}
+                    <h1>{{$register_page_section->title}}</h1>
+                    {!! $register_page_section->description !!}
                 </div>
                 <div class="row">
                     <div class="col-lg-6 mb-3">
