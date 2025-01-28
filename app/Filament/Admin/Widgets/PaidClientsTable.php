c7e6e00ac00726b32762477a9d1a395b307b4d02
tpct <eslamjackie@outlook.com>
2025-01-28 16:57:10 +0200

    first commit

---
+++ app/Filament/Admin/Widgets/PaidClientsTable.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filament\Admin\Widgets;
+
+use App\Models\Client;
+use BezhanSalleh\FilamentShield\Traits\HasWidgetShield;
+use Carbon\Carbon;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class PaidClientsTable extends BaseWidget
+{
+    use HasWidgetShield;
+
+    protected int | string | array $columnSpan = 'full';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(function(){
+                return Client::query()->where(function ($query) {
+                    $query->whereHas('services', function ($query) {
+                        $query->where('client_services.created_at', '=', Carbon::today()->toDateString());
+                    })->orWhereHas('packages', function ($query) {
+                        $query->where('client_packages.created_at', '=', Carbon::today()->toDateString());
+                    });
+                });
+            })
+            ->columns([
+                Tables\Columns\TextColumn::make('full_name')
+                    ->label(__("Name"))
+                    ->searchable(),
+                TextColumn::make('phone')
+                    ->label(__("Phone"))
+                    ->searchable(),
+                Tables\Columns\TextColumn::make("email")
+                    ->label(__("Email"))
+                    ->searchable(),
+            ]);
+    }
+}
