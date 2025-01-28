8c9f3d9a42572511e3ac6f9c997dbe4ee17e4df1
tpct <eslamjackie@outlook.com>
2025-01-28 15:43:52 +0200

    first commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,10 +23,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        SetLocale::class,
-        SetLanguage::class
     ];
 
     /**
@@ -36,6 +32,11 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+
+            \Illuminate\Session\Middleware\StartSession::class,
+            SetLocale::class,
+            SetLanguage::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
