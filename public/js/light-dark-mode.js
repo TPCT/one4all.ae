4ca26fc62e6eb96b0f21b7e7e3769c0c8d8c3853
tpct <eslamjackie@outlook.com>
2025-01-29 20:08:47 +0200

    first commit

--- public/js/light-dark-mode.js
+++ public/js/light-dark-mode.js
@@ -1,17 +1,17 @@
-$(document).ready(function () {
-  // Check local storage for mode preference
-  const mode = localStorage.getItem("mode");
-  if (mode) {
-    $("body").addClass(mode);
-  }
-
-  $(".main-btn").on("click", function () {
-    $("body").removeClass("dark-mode").addClass("light-mode");
-    localStorage.setItem("mode", "light-mode");
-  });
-
-  $(".third-btn").on("click", function () {
-    $("body").removeClass("light-mode").addClass("dark-mode");
-    localStorage.setItem("mode", "dark-mode");
-  });
-});
+// $(document).ready(function () {
+//   // Check local storage for mode preference
+//   const mode = localStorage.getItem("mode");
+//   if (mode) {
+//     $("body").addClass(mode);
+//   }
+//
+//   $(".main-btn").on("click", function () {
+//     $("body").removeClass("dark-mode").addClass("light-mode");
+//     localStorage.setItem("mode", "light-mode");
+//   });
+//
+//   $(".third-btn").on("click", function () {
+//     $("body").removeClass("light-mode").addClass("dark-mode");
+//     localStorage.setItem("mode", "dark-mode");
+//   });
+// });
