6ac679b6bc05439f92b20c886ce6ac26e6ae86cc
tpct <eslamjackie@outlook.com>
2025-01-29 02:41:41 +0200

    first commit

--- app/Helpers/TradingViewIndicators.php
+++ app/Helpers/TradingViewIndicators.php
@@ -210,10 +210,13 @@ class TradingViewIndicators
                 continue;
             $pairs[] = [$value, $close_price];
         }
+
+        $maos = [];
         foreach ($pairs as $pair) {
             $mao = $pair[1] - $pair[0];
             $precision = self::get_precision($pair[1]);
             $mao = round($mao, $precision);
+            $maos[] = $mao;
             if ($mao > 0)
                 $signals['BUY'] += 1;
             elseif ($mao < 0)
@@ -222,7 +225,7 @@ class TradingViewIndicators
                 $signals['NEUTRAL'] += 1;
         }
 
-        dd($signals);
+        dd($signals, $maos);
 
         return [
             'signal' => array_keys($signals, max($signals))[0],
