47fd076dcb46e098f9ed30ef635737dd3afc3fc8
tpct <eslamjackie@outlook.com>
2025-02-04 00:51:27 +0200

    first commit

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,72 +1,3 @@
-<!-- <footer class="main-footer-container">
-    <div class="container footer-section">
-        <div class="footer-item">
-            <div class="footer-item-header">
-                <a href="{{route('site.index')}}">
-                    <x-curator-glider :media="$logo" class="mw-100 mb-5" />
-                </a>
-            </div>
-            <div>
-                {!! $footer_description !!}
-            </div>
-        </div>
-        @foreach($menu->links as $index => $child)
-            @continue(!$child->status || !$child->has_children())
-            <div class="footer-item">
-                <div class="footer-item-header">
-                    <h5>{{$child->title}}</h5>
-                    <button
-                            class="toggleButton mobile-responsive"
-                            data-target="footer-link-{{$index}}"
-                    >
-                        <i class="fa-solid fa-plus"></i>
-                        <i class="fa-solid fa-minus" style="display: none"></i>
-                    </button>
-                </div>
-                <ul id="footer-link-{{$index}}">
-                    @foreach($child->children as $grandson)
-                        <li>
-                            <a href="{{$grandson->link}}">{{$grandson->title}}</a>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-        @endforeach
-
-    <div class="footer-copyright-container">
-        <div class="footer-copyright-container-centered">
-            <p class="d-flex flex-column align-items-center">@lang('site.copyright', ['year' => \Illuminate\Support\Carbon::now()->year, 'url' => '<a href="https://shiftcodes.net">ShiftCodes</a>'])</p>
-            <div class="social-media-icons">
-                @if ($twitter)
-                    <a href="{{$twitter}}" target="_blank" rel="noopener noreferrer">
-                        <i class="fa-brands fa-twitter"></i>
-                    </a>
-                @endif
-                @if ($linkedin)
-                    <a href="{{$linkedin}}" target="_blank" rel="noopener noreferrer">
-                        <i class="fa-brands fa-linkedin-in"></i>
-                    </a>
-                @endif
-                @if ($instagram)
-                    <a href="{{$instagram}}" target="_blank" rel="noopener noreferrer">
-                        <i class="fa-brands fa-instagram"></i>
-                    </a>
-                @endif
-                @if ($youtube)
-                    <a href="{{$youtube}}" target="_blank" rel="noopener noreferrer">
-                        <i class="fa-brands fa-youtube"></i>
-                    </a>
-                @endif
-                @if ($facebook)
-                    <a href="{{$facebook}}" target="_blank" rel="noopener noreferrer">
-                        <i class="fa-brands fa-facebook-f"></i>
-                    </a>
-                @endif
-            </div>
-        </div>
-    </div>
-</footer> -->
-
 <footer class="main-footer-container">
       <div class="container footer-section">
         <div class="footer-item">
@@ -80,8 +11,8 @@
               </div>
 
               @if ($newsletter_section)
-                    <div class="home-6th-section p-0">
-                        <div class="d-flex align-items-start  flex-column h-100 py-4">
+                    <div class="home-6th-section p-0" id="newsletter-container">
+                        <div class="d-flex align-items-start flex-column h-100 py-4">
                             <div>
                                 <h5>{{$newsletter_section->title}}</h5>
                                 <p>
@@ -181,10 +112,7 @@
                     dataType: 'json',
                     success: function(res) {
                         if (res.success){
-                            form.remove();
-                            $("#newsletter-container").append(`
-                                <div class="d-flex justify-content-center"><span class="text-success">` + res.message + `</span></div>
-                            `);
+                            $("#newsletter-container").remove();
                         }else{
                             $("#newsletter-email-error").text(res.message).removeClass('d-none').addClass('d-flex')
                         }
