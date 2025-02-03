6f63e13ce664512bacad907c199a59ce3a69415e
Hossaamhassan <hossam.h.eldeeb@gmail.com>
2025-02-04 00:33:03 +0200

    Upload

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -139,12 +139,10 @@
           <p>طرق الدفع</p>
           <div class="social-media-icons">
             <a href="#">
-              <i class="fa-brands fa-paypal"></i>
+             <picture>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/64px-Bitcoin.svg.png" alt="" class="mw-100 w-100">
+             </picture>
             </a>
-            <a href="#">
-              <i class="fa-brands fa-bitcoin"></i>
-            </a>
-
           </div>
         </div>
         <div class="footer-copyright-container-centered">
