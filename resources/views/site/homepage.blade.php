072823414c7ce5884c947c1cc8ff6989f3b87513
tpct <eslamjackie@outlook.com>
2025-02-05 01:03:06 +0200

    first commit

--- resources/views/site/homepage.blade.php
+++ resources/views/site/homepage.blade.php
@@ -53,6 +53,41 @@
                 </div>
             </div>
         </section>
+        <div class="tradingview-widget-container">
+            <div class="tradingview-widget-container__widget"></div>
+            <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+                {
+                    "symbols": [
+                    {
+                        "proName": "FOREXCOM:SPXUSD",
+                        "title": "S&P 500 Index"
+                    },
+                    {
+                        "proName": "FOREXCOM:NSXUSD",
+                        "title": "US 100 Cash CFD"
+                    },
+                    {
+                        "proName": "FX_IDC:EURUSD",
+                        "title": "EUR to USD"
+                    },
+                    {
+                        "proName": "BITSTAMP:BTCUSD",
+                        "title": "Bitcoin"
+                    },
+                    {
+                        "proName": "BITSTAMP:ETHUSD",
+                        "title": "Ethereum"
+                    }
+                ],
+                    "showSymbolLogo": true,
+                    "isTransparent": false,
+                    "displayMode": "adaptive",
+                    "colorTheme": "{{session('mode', 'light')}}",
+                    "locale": "{{app()->getLocale() == "ar" ? "ar_AE" : "en"}}"
+                }
+            </script>
+        </div>
+        <!-- TradingView Widget END -->
     @endif
     @if ($about_us_section)
         <section class="home-2nd-section overflow-hidden">
