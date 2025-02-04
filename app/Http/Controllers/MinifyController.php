41fec741d2c422fd0281078bd8bbe7528b60344a
tpct <eslamjackie@outlook.com>
2025-02-04 18:30:33 +0200

    first commit

--- app/Http/Controllers/MinifyController.php
+++ app/Http/Controllers/MinifyController.php
@@ -15,9 +15,7 @@ class MinifyController extends Controller
         if (!is_file($path))
             abort(404);
         // Cache the checksum of the file
-        $checksum = Cache::remember("file_checksum:{$path}", 60 * 60, function () use ($path) {
-            return md5_file($path);
-        });
+        $checksum = md5_file($path);
 
         // Cache the file content based on the checksum
         $content = Cache::remember("file_content:{$checksum}", 60 * 60, function () use ($path, $any) {
